e3255d8ddc48400c80630420f73925b16d6eac16
Amas9nine <amasdina2@gmail.com>
2022-06-21 18:38:30 +0600

    added

--- JavaScript---Golden/DOMevents WORK in Google/j.js
+++ JavaScript---Golden/DOMevents WORK in Google/j.js
@@ -1,5 +1,22 @@
  const $btn = document.querySelector(`button`)
+ let $imges = document.querySelector(`.imges`)
+ let index = 0
 $btn.addEventListener(`click`,function(event){
-	event.preventDefault( )
+	event.preventDefault()
 	$btn.innerHTML = (`U ARE WORKING IN GOOGLE GET MY CONGRATULATIONS `)
+
+	// $imges.insertAdjacentHTML(`beforeend`,
+	// `
+	// <img src="https://i.pinimg.com/originals/07/be/89/07be892db48f498bb2d11701d3ca7e69.jpg" alt="">
+
+	// `
+	// )
+	if(index = 5) {
+		index=0
+	$imges.classList.remove(`f${5}`)	
+	}
+	index=+1
+	$imges.classList.add(`f${index}`)
+	$imges.classList.remove(`f${index-1}`)
 })
+
