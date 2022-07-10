657955201b9345a2774fae237d5875332f36c759
Amas9nine <amasdina2@gmail.com>
2022-07-10 10:00:03 +0600

    added HOVER on the cards

--- boris-elcin/src/menu/card.jsx
+++ boris-elcin/src/menu/card.jsx
@@ -5,22 +5,25 @@ function Card(props) {
 	// const onDelete = () => {
 	// 	props.onDelete(props.id)
 	// }
-	
+
 	return (
 		<div className='area_of_card'>
 			<div className='card'>
 				<img src={props.img} />
-				<p>
-					{props.p}
-				</p>
+				<div className='text'>
+					<p>
+						{props.p}
+					</p>
+				</div>
 				<br />
 				<div className='price'>
 					{props.price}
 				</div>
+				<br />
 				<div className='divBTN'>
 					<button  >Купить</button>
 				</div>
-				<hr/>
+				<hr />
 			</div>
 
 		</div>
--- boris-elcin/src/menu/menu.jsx
+++ boris-elcin/src/menu/menu.jsx
@@ -26,33 +26,33 @@ function Menu(props) {
           price={5000}
 
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./home2.jpg"
+          p={'Продаю 15km дом  в Бишкеке'}
+          price={25000}
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./hom3.jpg"
+          p={'Продаю 2km дом  в Бишкеке'}
+          price={2000}
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./home4.jfif"
+          p={'Продаю 20km дом  в Бишкеке'}
+          price={4500}
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./home5.jfif"
+          p={'Продаю 8km дом  в Бишкеке'}
+          price={7400}
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./home6.jfif"
+          p={'Продаю 12km дом  в Бишкеке'}
+          price={24000}
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./home7.jfif"
+          p={'Продаю 7km дом  в Бишкеке'}
+          price={6000}
         />
-        <Card img="./card_home.png"
-          p={'Продаю 10km дом  в Бишкеке'}
-          price={5000}
+        <Card img="./home8.jpg"
+          p={'Продаю 12km дом  в Бишкеке'}
+          price={12000}
         />
       </div>
 
